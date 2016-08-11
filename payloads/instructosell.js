ab505b0d2f7e46d8cde11aaf928f76bd308c0a72
rjmacarthy <richardmacarthy@hotmail.com>
2016-08-11 14:35:58 +0100

    Random price and date

--- payloads/instructosell.js
+++ payloads/instructosell.js
@@ -1,12 +1,13 @@
 //https://localapi.dezrez.com/api/progression/sales/instructtosell
 
 var instruct = (roleId) => {
+    var price = faker.finance.amount(100000, 7000000, 0);
     return {
         "RoleId": roleId,
-        "ActualMarketingPrice": 100000,
+        "ActualMarketingPrice": price,
         "TeamId": 3,
         "PutOnMarket": true,
-        "InstructedDateTime": "2016-08-08T11:25:55+0100",
+        "InstructedDateTime": new Date().toJSON(),
         "NegotiatorIds": [
             103
         ],
